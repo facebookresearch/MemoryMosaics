85eb22a2cdda1b590cf3a42a95eebe0df11ad754
Jianyu Zhang <jianyu@nyu.edu>
2024-04-23 16:24:20 -0400

    BabiStories generation code

---
+++ BabiStories/data/filter_name.py
@@ -0,0 +1,22 @@
+names = []
+for i in range(128):
+	f = open(f'names{i}.txt', 'r')
+	data = f.readlines()
+	lines = []
+	for line in data:
+		split = line.find('. ')
+		if split == -1:
+			continue
+		name = line[split+2:].strip()+'\n'
+		if len(name) > 8:
+			continue
+		names.append(name)
+
+print(len(names))
+names = list(set(names))
+print(len(names))
+
+f = open('allnames.txt','w')
+
+f.writelines(names)
+f.close()
